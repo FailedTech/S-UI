1bb0d270da7dac40c918db53298b7645a6a9d0c0
FailedTech <noobengineer69@gmail.com>
2023-06-15 20:46:41 +0330

    6/15/2023

--- single_user_with_reality/autosetup.sh
+++ single_user_with_reality/autosetup.sh
@@ -9,6 +9,8 @@
 #Version:0.0.1
 #####################################################
 
+apt update && apt upgrade -y
+
 curl -Lo /root/sb https://github.com/SagerNet/sing-box/releases/download/v1.3-beta13/sing-box-1.3-beta13-linux-amd64.tar.gz &&
 tar -xzf /root/sb && cp -f /root/sing-box-*/sing-box /root && rm -r /root/sb /root/sing-box-* && chown root:root /root/sing-box &&
 chmod +x /root/sing-box
